6095409dcd441e453a9f824e9d9494b5f4961d3c
Leila <107024943+lakitse7@users.noreply.github.com>
2022-08-29 10:28:22 +0300

    Add files via upload

---
+++ word-blanks.js
@@ -0,0 +1,8 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + ".";
+// Only change code above this lin
