4823d5652774a42426d9749bb08b8368f84251a6
Leila <107024943+lakitse7@users.noreply.github.com>
2022-09-04 22:07:24 +0300

    Add files via upload

---
+++ local-scope-and-functions.js
@@ -0,0 +1,9 @@
+function myLocalScope() {
+  let myVar; // Only change code below this line
+  console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log('outside myLocalScope', myVar);
